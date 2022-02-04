d02677a472961b1e40a13b02103546a228b24864
MalikOwais123 <malikowaisjavaid@gmail.com>
2022-02-04 16:53:23 +0500

    sign up and sign in cleaning

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,69 +1,101 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux'
-import '../style/css/style.css'
-import { signin } from '../../Redux/auth/authActions'
-import SignOut from '../SignOut/SignOut'
-import GoogleBtn from '../GoogleBtn/GoogleBtn'
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import "../style/css/style.css";
+import { signin } from "../../Redux/auth/authActions";
+import SignOut from "../SignOut/SignOut";
+import GoogleBtn from "../GoogleBtn/GoogleBtn";
+import signinImage from "./../../images/login_SignUp/signin-image.png";
 
-const Login = ({signin}) => {
-    var [email , setEmail] = useState("")
-    var [password , setPassword] = useState("")
+const Login = ({ signin }) => {
+  var [email, setEmail] = useState("");
+  var [password, setPassword] = useState("");
 
-    var handleFormSubmit=(e)=>{
-        e.preventDefault();
-        var cred ={
-            email,
-            password
-        }
-        signin(cred)
-    }
-    return (
-        <div class="main">
-        <section class="sign-in">
+  var handleFormSubmit = (e) => {
+    e.preventDefault();
+    var cred = {
+      email,
+      password,
+    };
+    signin(cred);
+  };
+  return (
+    <div class="main">
+      <section class="sign-in">
         <div class="container">
-            <div class="signin-content">
-                <div class="signin-image">
-                    <figure>
-                    <img src={require("../../images/login_SignUp/signin-image.jpg")} alt="sing up image"></img></figure>
-                    <a href="/signup" class="signup-image-link">Create an account</a>
-                </div>
+          <div class="signin-content">
+            <div class="signin-image">
+              <figure>
+                <img src={signinImage} alt="sing up image"></img>
+              </figure>
+              <a href="/signup" class="signup-image-link">
+                Create an account
+              </a>
+            </div>
 
-                <div class="signin-form">
-                    <h2 class="form-title">Log In</h2>
-                    <form onSubmit={handleFormSubmit} method="POST" class="register-form" id="login-form">
-                        <div class="form-group">
-                            <label for="your_name"><i class="fas fa-envelope" style={{fontSize: '15px'}}></i></label>
-                            <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)} name="your_name" id="your_name" placeholder="Your Email"/>
-                        </div>
-                        <div class="form-group">
-                            <label for="your_pass"><i class="fas fa-lock" style={{fontSize: '15px'}}></i></label>
-                            <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} name="your_pass" id="your_pass" placeholder="Password"/>
-                        </div>
-                        {/* <div class="form-group">
+            <div class="signin-form">
+              <h2 class="form-title">Log In</h2>
+              <form
+                onSubmit={handleFormSubmit}
+                method="POST"
+                class="register-form"
+                id="login-form">
+                <div class="form-group">
+                  <label for="your_name">
+                    <i class="fas fa-envelope" style={{ fontSize: "15px" }}></i>
+                  </label>
+                  <input
+                    type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    name="your_name"
+                    id="your_name"
+                    placeholder="Your Email"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="your_pass">
+                    <i class="fas fa-lock" style={{ fontSize: "15px" }}></i>
+                  </label>
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    name="your_pass"
+                    id="your_pass"
+                    placeholder="Password"
+                  />
+                </div>
+                {/* <div class="form-group">
                             <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
                             <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
                         </div> */}
-                        <div class="form-group form-button">
-                            <input type="submit" name="signin" id="signin" class="form-submit" value="Log in"/>
-                        </div>
-                    </form>
-                    <div class="social-login">
-                        <span class="social-label">Or login with</span>
-                        <ul class="socials">
-                        <GoogleBtn />
-                            {/* <li><a href="#"><i class="fab fa-google" style={{fontSize: '25px'}}></i></a></li> */}
-                        </ul>
-                    </div>
+                <div class="form-group form-button">
+                  <input
+                    type="submit"
+                    name="signin"
+                    id="signin"
+                    class="form-submit"
+                    value="Log in"
+                  />
                 </div>
+              </form>
+              <div class="social-login">
+                <span class="social-label">Or login with</span>
+                <ul class="socials">
+                  <GoogleBtn />
+                  {/* <li><a href="#"><i class="fab fa-google" style={{fontSize: '25px'}}></i></a></li> */}
+                </ul>
+              </div>
             </div>
+          </div>
         </div>
-    </section>
-</div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
 var actions = {
-    signin
-}
+  signin,
+};
 
-export default connect(null,actions)(Login)
+export default connect(null, actions)(Login);
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,68 +1,110 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux'
-import { auth } from '../../Firebase/firebase'
-import { signup } from '../../Redux/auth/authActions'
-import '../style/css/style.css'
-const SignUp = ({signup}) => {
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { auth } from "../../Firebase/firebase";
+import { signup } from "../../Redux/auth/authActions";
+import "../style/css/style.css";
+import signupImage from "./../../images/login_SignUp/signup-image1.png";
+const SignUp = ({ signup }) => {
+  var [fullName, setFullName] = useState("");
+  var [email, setEmail] = useState("");
+  var [password, setPassword] = useState("");
 
-    var [fullName , setFullName] = useState("")
-    var [email , setEmail] = useState("")
-    var [password , setPassword] = useState("")
-
-    var handleFormSubmit=(e)=>{
-        e.preventDefault();
-       var cred ={
-            fullName,
-            email,
-            password
-        }
-        signup(cred)
-    }
-return (
+  var handleFormSubmit = (e) => {
+    e.preventDefault();
+    var cred = {
+      fullName,
+      email,
+      password,
+    };
+    signup(cred);
+  };
+  return (
     <div>
-    <div class="main">
-    <section class="signup">
-    <div class="container">
-        <div class="signup-content">
-            <div class="signup-form">
+      <div class="main">
+        <section class="signup">
+          <div class="container">
+            <div class="signup-content">
+              <div class="signup-form">
                 <h2 class="form-title">Sign up</h2>
-                <form onSubmit={handleFormSubmit} method="POST" class="register-form" id="register-form">
-                    <div class="form-group">
-                        <label for="name"><i class="fas fa-user" style={{fontSize: '15px'}}></i></label>
-                        <input type="text" value={fullName} onChange={(e)=>setFullName(e.target.value)} name="name" id="name" placeholder="Your Name"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="email"><i class="fas fa-envelope" style={{fontSize: '15px'}}></i></label>
-                        <input type="email"  value={email} onChange={(e)=>setEmail(e.target.value)} name="email" id="email" placeholder="Your Email"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="pass"><i class="fas fa-lock" style={{fontSize: '15px'}}></i></label>
-                        <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} name="pass" id="pass" placeholder="Password"/>
-                    </div>
-                    {/* <div class="form-group">
+                <form
+                  onSubmit={handleFormSubmit}
+                  method="POST"
+                  class="register-form"
+                  id="register-form">
+                  <div class="form-group">
+                    <label for="name">
+                      <i class="fas fa-user" style={{ fontSize: "15px" }}></i>
+                    </label>
+                    <input
+                      type="text"
+                      value={fullName}
+                      onChange={(e) => setFullName(e.target.value)}
+                      name="name"
+                      id="name"
+                      placeholder="Your Name"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="email">
+                      <i
+                        class="fas fa-envelope"
+                        style={{ fontSize: "15px" }}></i>
+                    </label>
+                    <input
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      name="email"
+                      id="email"
+                      placeholder="Your Email"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="pass">
+                      <i class="fas fa-lock" style={{ fontSize: "15px" }}></i>
+                    </label>
+                    <input
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      name="pass"
+                      id="pass"
+                      placeholder="Password"
+                    />
+                  </div>
+                  {/* <div class="form-group">
                         <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
                         <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
                     </div> */}
-                    <div class="form-group form-button">
-                        <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
-                    </div>
+                  <div class="form-group form-button">
+                    <input
+                      type="submit"
+                      name="signup"
+                      id="signup"
+                      class="form-submit"
+                      value="Register"
+                    />
+                  </div>
                 </form>
-            </div>
-            <div class="signup-image">
+              </div>
+              <div class="signup-image">
                 <figure>
-                    <img  src={require("../../images/login_SignUp/signup-image.jpg")} alt="sing up image"></img></figure>
-                <a href="/login" class="signup-image-link">I am already member</a>
+                  <img src={signupImage} alt="sing up image"></img>
+                </figure>
+                <a href="/login" class="signup-image-link">
+                  I am already member
+                </a>
+              </div>
             </div>
-        </div>
+          </div>
+        </section>
+      </div>
     </div>
-</section>
-    </div>
-        </div>
-    )
-}
+  );
+};
 
 var actions = {
-    signup
-}
+  signup,
+};
 
-export default connect(null,actions)(SignUp)
+export default connect(null, actions)(SignUp);
