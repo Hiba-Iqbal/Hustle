1b1d588769c6a3b3650a2026ab4ede8e8aabce58
MalikOwais123 <malikowaisjavaid@gmail.com>
2022-02-06 20:37:54 +0500

    cookie added with home page navigation need to implement while user logged in

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
 import React, { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
-import signUp from './components/SignUp/SignUp'
-import Login from './components/Login/Login'
+import signUp from "./components/SignUp/SignUp";
+import Login from "./components/Login/Login";
 import Home from "./Home";
 import { connect } from "react-redux";
-import {firebaseAuthListener} from './Redux/auth/authActions'
+import { firebaseAuthListener } from "./Redux/auth/authActions";
+import history from "./history/history";
+import { useCookies } from "react-cookie";
 
-function App ({firebaseAuthListener}) {
+function App({ firebaseAuthListener }) {
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+  useEffect(() => {
+    // TODO
+    // HOW TO SET COOKIE
+    // setCookie("user", "token" ,{ path: "/" });
 
-  useEffect(()=>{
-    firebaseAuthListener()
-  },[firebaseAuthListener])
+    // HOW TO REMOVE COOKIE
+    // removeCookie("user");
+
+    if (cookies.user) {
+      history.push("/");
+    } else {
+      history.push("/login");
+    }
+  }, []);
+
+  useEffect(() => {
+    firebaseAuthListener();
+  }, [firebaseAuthListener]);
 
   return (
     <div className="App">
       <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/signup" component={signUp}/>
-      <Route exact path="/login" component={Login}/>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={signUp} />
+        <Route exact path="/login" component={Login} />
       </Switch>
     </div>
-    )
-  
-};
-
-var actions ={
-  firebaseAuthListener
+  );
 }
 
-export default connect(null,actions)(App);
+var actions = {
+  firebaseAuthListener,
+};
+
+export default connect(null, actions)(App);
